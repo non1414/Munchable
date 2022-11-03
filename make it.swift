9974edda215e9226906a306c3d7deb39a4c2c4a1
nhajarn <alruqi.hajar@gmail.com>
2022-11-03 11:34:03 +0300

    add makeIt page

--- make it.swift
+++ make it.swift
@@ -7,9 +7,204 @@
 
 import SwiftUI
 
+
+struct GrowingSelected: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+//            .background(Color(uiColor: UIColor(red: 166/255, green: 147/255, blue: 143/255, alpha: 100)))
+//        RoundedRectangle(cornerRadius: 24 )
+            .background(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+            .foregroundColor(.white)
+            .frame(width: 132, height: 35)
+        Text("Turnips").foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+
+
 struct make_it: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            ZStack  {
+                Color(uiColor: UIColor(red: 235/255, green: 232/255, blue: 231/255, alpha: 100))
+                VStack{
+                    Text("What do you have in the fridge ?").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                    
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("VEGETABLES").foregroundColor(.white)
+                        }
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("FRUITS").foregroundColor(.white)
+                        }             }
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("VEGETABLES").foregroundColor(.white)
+                        }
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("FRUITS").foregroundColor(.white)
+                        }             }
+                    HStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("RED MEAT").foregroundColor(.white)
+                        }
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 24 ).frame(width: 134, height: 96).foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                            Text("POULRTY").foregroundColor(.white)
+                        }
+                        
+                        
+                        
+                    }
+                    VStack{
+                        HStack{
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                            }
+                            
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                            }
+                            
+                        }
+                        HStack{
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                            }
+                            
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .stroke(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)), lineWidth: 1)
+                                        .frame(width: 132, height: 35)
+                                        .foregroundColor(Color.black.opacity(0.5))
+                                    
+                                    Text("Turnips").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                }
+                            }
+                            
+                        }
+                    }
+                    Text("Your Selection:").foregroundColor(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                    
+                    VStack{
+                        HStack{
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .fill(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                        .frame(width: 132, height: 35)
+                                    Text("Turnips").foregroundColor(.white)
+                                }
+                                
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .fill(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                        .frame(width: 132, height: 35)
+                                    Text("Turnips").foregroundColor(.white)
+                                }
+                            }
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .fill(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                        .frame(width: 132, height: 35)
+                                    Text("Turnips").foregroundColor(.white)
+                                }
+                                
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .fill(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                        .frame(width: 132, height: 35)
+                                    Text("Turnips").foregroundColor(.white)
+                                }
+                            }
+                        }
+                        HStack{
+                            VStack{
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 24 )
+                                        .fill(Color(uiColor: UIColor(red: 35/255, green: 56/255, blue: 84/255, alpha: 100)))
+                                        .frame(width: 132, height: 35)
+                                    Text("Turnips").foregroundColor(.white)
+                                }
+                            }
+                        }
+                    }
+                    
+                    Button("Make My Dish") {
+                        
+                    }.buttonStyle(GrowingButton())
+                }
+                
+            }
+            //
+        }
     }
 }
 
